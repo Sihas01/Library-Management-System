f3ee7122296185cb550ebb8790b7a64ad4decb1a
Sihas Senevirathne <sihasmandipa77@gmail.com>
2024-12-29 07:59:11 -0800

    Add Person and Member classes for user management

--- Library Management System/Model/Member.cs
+++ Library Management System/Model/Member.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Library_Management_System.Model
 {
-    internal class Member
+    internal class Member:Person
     {
+        public Member(string memberId,string name,string email,string phoneNumber):base(memberId,name,email,phoneNumber) { }
+
+
+
+        
     }
 }
---
+++ Library Management System/Model/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System.Model
+{
+    internal class Person
+    {
+        private string _id;
+        private string _name;
+        private string _email;
+        private string _phoneNumber;
+
+        public Person(string id, string name, string email, string phoneNumber)
+        {
+            _id = id;
+            _name = name;
+            _email = email;
+            _phoneNumber = phoneNumber;
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public String Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+    }
+}
