0a95a06fb200a8fc734f739a3517b19f11786b6e
Sihas Senevirathne <sihasmandipa77@gmail.com>
2025-01-03 03:16:58 -0800

    create user validation class and add basic validations

--- Library Management System/Form1.cs
+++ Library Management System/Form1.cs
@@ -12,7 +12,7 @@ namespace Library_Management_System
 
         public void Form1_Load(object sender, EventArgs e) 
         {
-            Member member1 = new Member("usero1", "John", "john@gmail.com", "0751171926");
+            Member member1 = new Member("usero1", "John", "john@gmail.com", "0751171926","1234");
             label1.Text = member1.Name;
             label2.Text = member1.Id;
         }
