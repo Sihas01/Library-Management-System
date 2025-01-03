3cd5a5c4fc2a88a590ea745e60ddf7723d33aeca
Sihas Senevirathne <sihasmandipa77@gmail.com>
2025-01-03 03:29:52 -0800

    create memberVIew winform

--- Library Management System/Form1.cs
+++ Library Management System/Form1.cs
@@ -7,14 +7,14 @@ namespace Library_Management_System
         public Form1()
         {
             InitializeComponent();
-            Form1_Load(null, EventArgs.Empty);
+            //Form1_Load(null, EventArgs.Empty);
         }
 
-        public void Form1_Load(object sender, EventArgs e) 
-        {
-            Member member1 = new Member("usero1", "John", "john@gmail.com", "0751171926","1234");
-            label1.Text = member1.Name;
-            label2.Text = member1.Id;
-        }
+        //public void Form1_Load(object sender, EventArgs e) 
+        //{
+        //    Member member1 = new Member("usero1", "John", "john@gmail.com", "0751171926","1234");
+        //    label1.Text = member1.Name;
+        //    label2.Text = member1.Id;
+        //}
     }
 }
--- Library Management System/Program.cs
+++ Library Management System/Program.cs
@@ -1,3 +1,5 @@
+using Library_Management_System.View;
+
 namespace Library_Management_System
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Library_Management_System
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MemberView());
         }
     }
 }
