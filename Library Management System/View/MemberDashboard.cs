e7738908568c7471f2809f1b22ae7762769a07eb
Sihas Senevirathne <sihasmandipa77@gmail.com>
2025-01-31 05:58:44 -0800

    Create Member Dashboard Designs

---
+++ Library Management System/View/MemberDashboard.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management_System.View
+{
+    public partial class MemberDashboard : Form
+    {
+        BorrowBookForm borrowBookForm;
+        ReturnBookForm returnBookForm;
+        ReserveBookForm reserveBookForm;
+        FineForm fineForm;
+        MemeberDashboardView memeberDashboardView;
+        public MemberDashboard()
+        {
+            InitializeComponent();
+            borrowBookForm = null;
+            returnBookForm = null;
+            reserveBookForm = null;
+            fineForm = null;
+            memeberDashboardView = null;
+            this.Load += MemberDashboard_Load;
+
+        }
+        private void OpenDashboard()
+        {
+    
+            if (memeberDashboardView == null || memeberDashboardView.IsDisposed)
+            {
+                memeberDashboardView = new MemeberDashboardView();
+                memeberDashboardView.FormClosed += memberDashboard_Closed;
+                memeberDashboardView.MdiParent = this;
+                memeberDashboardView.Dock = DockStyle.Fill;
+                memeberDashboardView.Show();
+            }
+            else
+            {
+
+                memeberDashboardView.Activate();
+            }
+        }
+
+        private void memberDashboard_Closed(object sender, FormClosedEventArgs e)
+        {
+            memeberDashboardView.Dispose();
+            memeberDashboardView = null;
+        }
+
+        private void MemberDashboard_Load(object sender, EventArgs e)
+        {
+            OpenDashboard();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenDashboard();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (borrowBookForm == null || borrowBookForm.IsDisposed)
+            {
+                borrowBookForm = new BorrowBookForm();
+                borrowBookForm.FormClosed += borrowBookForm_Closed;
+                borrowBookForm.MdiParent = this;
+                borrowBookForm.Dock = DockStyle.Fill;
+                borrowBookForm.Show();
+            }
+            else
+            {
+                borrowBookForm.Activate();
+            }
+        }
+
+        private void borrowBookForm_Closed(object sender, FormClosedEventArgs e)
+        {
+            borrowBookForm.Dispose();
+            borrowBookForm = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (returnBookForm == null || returnBookForm.IsDisposed)
+            {
+                returnBookForm = new ReturnBookForm();
+                returnBookForm.FormClosed += returnBookForm_Closed;
+                returnBookForm.MdiParent = this;
+                returnBookForm.Dock = DockStyle.Fill;
+                returnBookForm.Show();
+            }
+            else
+            {
+                returnBookForm.Activate();
+            }
+        }
+
+        private void returnBookForm_Closed(object sender, FormClosedEventArgs e)
+        {
+            returnBookForm.Dispose();
+            returnBookForm = null;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (reserveBookForm == null || reserveBookForm.IsDisposed)
+            {
+                reserveBookForm = new ReserveBookForm();
+                reserveBookForm.FormClosed += reserveBookForm_Closed;
+                reserveBookForm.MdiParent = this;
+                reserveBookForm.Dock = DockStyle.Fill;
+                reserveBookForm.Show();
+            }
+            else
+            {
+                reserveBookForm.Activate();
+            }
+        }
+
+        private void reserveBookForm_Closed(object sender, FormClosedEventArgs e)
+        {
+            reserveBookForm.Dispose();
+            reserveBookForm = null;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (fineForm == null || fineForm.IsDisposed)
+            {
+                fineForm = new FineForm();
+                fineForm.FormClosed += FineForm_Closed;
+                fineForm.MdiParent = this;
+                fineForm.Dock = DockStyle.Fill;
+                fineForm.Show();
+            }
+            else
+            {
+                fineForm.Activate();
+            }
+        }
+
+        private void FineForm_Closed(object sender, FormClosedEventArgs e)
+        {
+            fineForm.Dispose();
+            fineForm = null;
+        }
+    }
+}
---
+++ Library Management System/View/ReserveBookForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Library_Management_System.View
+{
+    partial class ReserveBookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            hopeButton3 = new ReaLTaiizor.Controls.HopeButton();
+            poisonDataGridView1 = new ReaLTaiizor.Controls.PoisonDataGridView();
+            search = new ReaLTaiizor.Controls.BigTextBox();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)poisonDataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // hopeButton3
+            // 
+            hopeButton3.BorderColor = Color.FromArgb(220, 223, 230);
+            hopeButton3.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            hopeButton3.DangerColor = Color.FromArgb(245, 108, 108);
+            hopeButton3.DefaultColor = Color.FromArgb(255, 255, 255);
+            hopeButton3.Font = new Font("Segoe UI", 12F);
+            hopeButton3.HoverTextColor = Color.FromArgb(48, 49, 51);
+            hopeButton3.InfoColor = Color.FromArgb(144, 147, 153);
+            hopeButton3.Location = new Point(40, 454);
+            hopeButton3.Name = "hopeButton3";
+            hopeButton3.PrimaryColor = Color.FromArgb(2, 39, 74);
+            hopeButton3.Size = new Size(293, 40);
+            hopeButton3.SuccessColor = Color.FromArgb(103, 194, 58);
+            hopeButton3.TabIndex = 24;
+            hopeButton3.Text = "Reserve Book";
+            hopeButton3.TextColor = Color.White;
+            hopeButton3.WarningColor = Color.FromArgb(230, 162, 60);
+            // 
+            // poisonDataGridView1
+            // 
+            poisonDataGridView1.AllowUserToAddRows = false;
+            poisonDataGridView1.AllowUserToDeleteRows = false;
+            poisonDataGridView1.AllowUserToResizeColumns = false;
+            poisonDataGridView1.AllowUserToResizeRows = false;
+            poisonDataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            poisonDataGridView1.BackgroundColor = Color.FromArgb(255, 255, 255);
+            poisonDataGridView1.BorderStyle = BorderStyle.None;
+            poisonDataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            poisonDataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(12, 72, 128);
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = Color.FromArgb(255, 255, 255);
+            dataGridViewCellStyle1.Padding = new Padding(10);
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(12, 72, 128);
+            dataGridViewCellStyle1.SelectionForeColor = Color.FromArgb(17, 17, 17);
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            poisonDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            poisonDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(255, 255, 255);
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = Color.FromArgb(136, 136, 136);
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(57, 142, 222);
+            dataGridViewCellStyle2.SelectionForeColor = Color.White;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            poisonDataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            poisonDataGridView1.EnableHeadersVisualStyles = false;
+            poisonDataGridView1.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            poisonDataGridView1.GridColor = Color.FromArgb(255, 255, 255);
+            poisonDataGridView1.Location = new Point(40, 123);
+            poisonDataGridView1.Name = "poisonDataGridView1";
+            poisonDataGridView1.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(12, 72, 128);
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = Color.FromArgb(255, 255, 255);
+            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(12, 72, 128);
+            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(17, 17, 17);
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            poisonDataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            poisonDataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            poisonDataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            poisonDataGridView1.Size = new Size(779, 295);
+            poisonDataGridView1.TabIndex = 23;
+            // 
+            // search
+            // 
+            search.BackColor = Color.Transparent;
+            search.Font = new Font("Tahoma", 11F);
+            search.ForeColor = Color.DimGray;
+            search.Image = null;
+            search.Location = new Point(40, 53);
+            search.MaxLength = 32767;
+            search.Multiline = false;
+            search.Name = "search";
+            search.ReadOnly = false;
+            search.Size = new Size(779, 41);
+            search.TabIndex = 22;
+            search.Text = "Search";
+            search.TextAlignment = HorizontalAlignment.Left;
+            search.UseSystemPasswordChar = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(2, 39, 74);
+            panel1.Location = new Point(-3, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(891, 29);
+            panel1.TabIndex = 21;
+            // 
+            // ReserveBookForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(884, 561);
+            Controls.Add(hopeButton3);
+            Controls.Add(poisonDataGridView1);
+            Controls.Add(search);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ReserveBookForm";
+            Text = "ReserveBookForm";
+            ((System.ComponentModel.ISupportInitialize)poisonDataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Controls.HopeButton hopeButton3;
+        private ReaLTaiizor.Controls.PoisonDataGridView poisonDataGridView1;
+        private ReaLTaiizor.Controls.BigTextBox search;
+        private Panel panel1;
+    }
+}
