6be460ee2fb46c25f003de89f38a8b40bffc9bad
Manuja-15 <87766537+Manuja-15@users.noreply.github.com>
2025-01-31 08:41:31 -0800

    add interface

---
+++ Library Management System/View/IBookView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library_Management_System.Model;
+
+namespace Library_Management_System.View
+{
+    internal interface IBookView
+    {
+        string Title { get; set; }
+        string Author { get; set; }
+        string ISBN { get; set; }
+        string Genre { get; set; }
+        void ShowMessage(string message);
+        void DisplayBooks(List<Book> books);
+    }
+}
